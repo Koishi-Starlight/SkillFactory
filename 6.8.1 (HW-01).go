69c13733b11881d7fbd6fa588a95179338c8cf6a
Koishi <136453551+Koishi-Starlight@users.noreply.github.com>
2024-01-24 22:00:31 +1100

    Update 6.8.1 (HW-01).go

--- 6.8.1 (HW-01).go
+++ 6.8.1 (HW-01).go
@@ -27,8 +27,8 @@ func getIntResult(num1, num2 float64, operator string) float64 { // Basic operat
 func main() {
 	reader := bufio.NewReader(os.Stdin) //read from the console, based on OS standard input
 	fmt.Println(`Enter mathematical operation in following format: "a + b, a - b, a * b, a / b"'`)
-	text, _ := reader.ReadString('\n')         //Awaiting input
-	text = strings.TrimSpace(text)             //Removing \n from input
+	text, _ := reader.ReadString('\n') 
+	text = strings.TrimSpace(text)
 	operationSlice := strings.Split(text, " ") //Making a slice, uses "spaces" as separator
 	if len(operationSlice) != 3 {              //Checks if you entered correct amount of arguments. 2 numbers and 1 operator
 		panic("Incorrect amount of arguments.")
@@ -36,8 +36,8 @@ func main() {
 	num1 := operationSlice[0] //Assigning numbers and operators after slicing the text.
 	num2 := operationSlice[2]
 	operator := operationSlice[1]
-	convInt1, _ := strconv.ParseFloat(num1, 64) // Int calculation part. I didn't use conversion then " if num > 0 && num < 11 " as checker,
-	convInt2, _ := strconv.ParseFloat(num2, 64) // cuz only 10 numbers are allowed, so I used a map instead.
+	convInt1, _ := strconv.ParseFloat(num1, 64) 
+	convInt2, _ := strconv.ParseFloat(num2, 64) 
 	if convInt2 == 0 && operator == "/" {
 		panic("Error division")
 	}
